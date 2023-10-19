ac8cc448c0114395b92caed2c335b182e7147d52
yourname <rugor@users.noreply.github.com>
2023-10-19 12:58:28 -0700

    added hotjar

--- wp-content/themes/grunt/header.php
+++ wp-content/themes/grunt/header.php
@@ -48,6 +48,17 @@
     src="https://www.facebook.com/tr?id=2250091545085722&ev=PageView&noscript=1"
   /></noscript>
   <!-- End Facebook Pixel Code -->
+  <!-- Hotjar Tracking Code for https://grunt.ca/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3702034,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </head>
 
 <body <?php body_class(); ?>>
